b6094c1321fad50715d2aaa844ab5463e4b9adf4
Sreemathi2004 <sreemathik.m2004@gmail.com>
2024-06-25 09:44:36 +0530

    commit

---
+++ project1/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import { LearnComponent } from './Components/LearnComponent';
+
+
+
+function App() {
+  return <div>Sree
+    <LearnComponent/>
+  </div>
+};
+
+export default App;
